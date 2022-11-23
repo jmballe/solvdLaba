ffda557a2b2fbc75f7dab2d864a93336150a5996
Juan Manuel <jm_balle@hotmail.com>
2022-11-23 19:44:27 -0300

    Changes in room and department

--- tareas/hospital/Deparment.java
+++ tareas/hospital/Deparment.java
@@ -1,4 +1,7 @@
 package hospital;
 
 public class Deparment {
+    private int room;
+    private String Speciality;
+
 }
--- tareas/hospital/Room.java
+++ tareas/hospital/Room.java
@@ -2,8 +2,16 @@ package hospital;
 
 public class Room {
     private int id;
+    private double width;
+    private double length;
 
     public Room() {
+    }
+
+    public Room(int id, double width, double length) {
+        this.id = id;
+        this.width = width;
+        this.length = length;
     }
 
     public int getId() {
@@ -12,5 +20,21 @@ public class Room {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
     }
 }
