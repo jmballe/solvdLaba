6e1060ea6e4a82fd43674c81c08b3f7b2209b1bc
Juan Manuel <jm_balle@hotmail.com>
2022-11-23 19:08:42 -0300

    Added Homework2 in progress

---
+++ tareas/hospital/Nurse.java
@@ -0,0 +1,8 @@
+package hospital;
+
+public class Nurse extends Employee{
+    public Nurse(String name, int age, String sex, int id) {
+        super(name, age, sex, id);
+    }
+
+}
---
+++ tareas/hospital/Recepcionist.java
@@ -0,0 +1,16 @@
+package hospital;
+
+public class Recepcionist extends Employee{
+
+    public Recepcionist(String name, int age, String sex, int id) {
+        super(name, age, sex, id);
+    }
+
+    public void registerPatient(Hospital hospital,Patient patient){
+        hospital.addPatient(patient);
+    }
+
+    public void patientStatus(Patient patient){
+
+    }
+}
