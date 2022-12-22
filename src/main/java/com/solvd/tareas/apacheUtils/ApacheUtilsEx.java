72e2d122eb77ca67292ce6dd3eaad8242625d2a0
Juan Manuel <jm_balle@hotmail.com>
2022-12-22 14:21:09 -0300

    feat: Finished Apache Utils homework

--- src/main/java/com/solvd/tareas/apacheUtils/ApacheUtilsEx.java
+++ src/main/java/com/solvd/tareas/apacheUtils/ApacheUtilsEx.java
@@ -1,16 +1,33 @@
 package com.solvd.tareas.apacheUtils;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ApacheUtilsEx {
     public static void main(String[] args) {
-        File file = new File("text.txt");
-        String text = null;
         try {
-            text = FileUtils.readFileToString(file);
-        }catch (IOException e) {
+            String exitFile = System.getProperty("user.dir") + "\\src\\main\\java\\com\\solvd\\tareas\\apacheUtils\\salida";
+            FileUtils.forceDelete(new File(exitFile)); //Erase file if already exists since if file exists it appends words count again.
+
+            String text = FileUtils.readFileToString(new File(System.getProperty("user.dir") + "\\src\\main\\resources\\text.txt"),
+                    "UTF-8");
+
+            Map<String, Integer> wordCounts = new HashMap<>();
+
+            for (String word : StringUtils.split(text)) {
+                word = StringUtils.strip(word.toLowerCase(), ".");
+                wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+            }
+            for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+                FileUtils.writeStringToFile(new File(exitFile), entry.getKey() + ": " + entry.getValue() + "\n", "UTF-8", true);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
