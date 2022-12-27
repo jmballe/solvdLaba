b93246f706ff922f7baacd087afaac506ce0e75d
Juan Manuel <jm_balle@hotmail.com>
2022-12-27 13:47:20 -0300

    feat(ApacheUtils): Fewer lines.

--- src/main/java/com/solvd/tareas/apacheUtils/ApacheUtilsEx.java
+++ src/main/java/com/solvd/tareas/apacheUtils/ApacheUtilsEx.java
@@ -5,29 +5,31 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ApacheUtilsEx {
-    public static void main(String[] args) {
-        try {
-            String exitFile = System.getProperty("user.dir") + "\\src\\main\\java\\com\\solvd\\tareas\\apacheUtils\\salida";
-            FileUtils.forceDelete(new File(exitFile)); //Erase file if already exists since if file exists it appends words count again.
+    public static void main(String[] args) throws IOException{
+            FileUtils.forceDelete(new File(System.getProperty("user.dir") +
+                    "\\src\\main\\java\\com\\solvd\\tareas\\apacheUtils\\salida")); //Erase file if already exists since if file exists it appends words count again.
 
             String text = FileUtils.readFileToString(new File(System.getProperty("user.dir") + "\\src\\main\\resources\\text.txt"),
                     "UTF-8");
 
-            Map<String, Integer> wordCounts = new HashMap<>();
 
-            for (String word : StringUtils.split(text)) {
-                word = StringUtils.strip(word.toLowerCase(), ".");
-                wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+            Map<String, Long> wordCounts = Arrays.stream((StringUtils.split(text)))
+                    .map(String::toLowerCase).map(w -> StringUtils.strip(w, ".,\"*-+[]{}()<>$%#@_¡!?¿/;:"))
+                    .collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+
+//            for (String word : StringUtils.split(text)) {
+//                word = StringUtils.strip(word.toLowerCase(), ".,\"*-+[]{}()<>$%#@_¡!?¿/;:");
+//                wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+//            }
+            for (Map.Entry<String, Long> entry : wordCounts.entrySet()) {
+                FileUtils.writeStringToFile(new File(System.getProperty("user.dir") +
+                                "\\src\\main\\java\\com\\solvd\\tareas\\apacheUtils\\salida"),
+                        entry.getKey() + ": " + entry.getValue() + "\n", "UTF-8", true);
             }
-            for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
-                FileUtils.writeStringToFile(new File(exitFile), entry.getKey() + ": " + entry.getValue() + "\n", "UTF-8", true);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
